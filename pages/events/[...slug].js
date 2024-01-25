9af6075f8d61a7f1ab881a47d249f11692b64bc7
Thuy Nguyen <vanthuy101102@gmail.com>
2024-01-26 05:22:10 +0700

    filter event by params

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,3 +1,19 @@
+import EventList from '@/components/events/event.list'
+import { getFilteredEvents } from '@/resources/static'
+import { useRouter } from 'next/router'
+
 export default function FilterEvents() {
-  return <div>FilterEvents</div>
+  const routes = useRouter()
+  const [year, month] = routes.query.slug
+  const dateFilter = [parseInt(year), parseInt(month)]
+
+  const events = getFilteredEvents(dateFilter)
+  if (!events || events.length === 0) {
+    return (
+      <p style={{ margin: '3rem' }} className='center'>
+        Not found event, try again
+      </p>
+    )
+  }
+  return <EventList items={events} />
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,3 +1,22 @@
-export default function EventsPage() {
-  return <div>EventsPage</div>
+import EventsSearch from '@/components/event-detail/event-search'
+import EventList from '@/components/events/event.list'
+import Metadata from '@/components/metadata'
+import { getAllEvents } from '@/resources/static'
+import { useRouter } from 'next/router'
+
+export default function EventPages() {
+  const allEvents = getAllEvents()
+  const routes = useRouter()
+
+  const onSearch = (year, month) => {
+    const fullPath = `/events/${year}/${month}`
+    routes.push(fullPath)
+  }
+  return (
+    <main>
+      <Metadata title='Explore all events' description='Page show all events' />
+      <EventsSearch onSearch={onSearch}></EventsSearch>
+      <EventList items={allEvents} />
+    </main>
+  )
 }
